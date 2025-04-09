9f157d6978cede7a065e940e06d4c0d30a592d46
Hamza Hanfi <hhanfi@smb.sqli.com>
2025-04-09 22:02:22 +0200

    fix/add control value accesor to input

--- src/app/ui/componets/input/input.component.ts
+++ src/app/ui/componets/input/input.component.ts
@@ -1,14 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ControlValueAccessor, FormControl } from '@angular/forms';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
   imports: [],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputComponent),
+    multi: true,
+  }],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() label = '';
   @Input() icon: string = '';
   @Input() tagColor = '';
@@ -22,19 +27,43 @@ export class InputComponent {
   @Output() focus = new EventEmitter<Event>();
   @Output() blur = new EventEmitter<Event>();
 
+  value: string = '';
+  isDisabled: boolean = false;
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+
   get iconPath(): string {
     return `assets/icons/${this.icon}.svg` ;
   }
 
-  onInput(event: Event): void {
-    this.input.emit(event);
+  writeValue(value: any): void {
+    this.value = value;
   }
 
-  onFocus(): void {
-    this.focus.emit();
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
   }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+
+
+  onInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+  }
+
 
   onBlur(): void {
-    this.input.emit();
+    this.onTouched();
   }
 }
