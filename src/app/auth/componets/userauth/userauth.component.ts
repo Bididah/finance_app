c75c15b8f3f13cdb8a9f590a88e4c790c32a34e9
Hamza hanfi <hanfihamza20@gmail.com>
2025-11-19 22:18:52 +0100

    feat: create userauth component

--- src/app/auth/componets/userauth/userauth.component.ts
+++ src/app/auth/componets/userauth/userauth.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AuthmodalComponent } from '../authmodal/authmodal.component';
 import { Subscription } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginRequest, SingUpRequest } from '../../interfaces';
 import { AuthService } from '../../../services/auth.service';
+import { AuthFormComponent } from '../authform/authform.component';
 
 @Component({
   selector: 'app-userauth',
   standalone: true,
-  imports: [AuthmodalComponent],
+  imports: [AuthFormComponent],
   templateUrl: './userauth.component.html',
   styleUrl: './userauth.component.scss',
 })
@@ -16,7 +16,7 @@ export class UserAuthComponent implements OnInit, OnDestroy {
   public type!: 'login' | 'sing up';
   public subscription = new Subscription();
 
-  constructor(private route: ActivatedRoute, private authService: AuthService) {}
+  constructor(private route: ActivatedRoute, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.subscription.add(
@@ -29,32 +29,20 @@ export class UserAuthComponent implements OnInit, OnDestroy {
   submit(event: SingUpRequest | LoginRequest) {
     if (this.type === 'login') {
       this.subscription.add(
-        this.authService.login(event as LoginRequest).subscribe(
-          (user) => {
-            console.log(
-              `Hey ${user.firstName} ${user.lastName} you are logged in`
-            );
-          },
-          (error) => {
-            console.log(error);
-          }
-        )
+        this.authService.login(event as LoginRequest).subscribe((data) => {
+          this.router.navigate(['/dashboard']);
+        })
       );
     } else {
       this.subscription.add(
-        this.authService.register(event as SingUpRequest).subscribe(
-          (user) => {
-            console.log(`Welcome ${user.firstName} ${user.lastName}`);
-          },
-          (error) => {
-            console.log(error);
-          }
-        )
+        this.authService.register(event as SingUpRequest).subscribe((data) => {
+          this.router.navigate(['/dashboard']);
+        })
       );
     }
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe;
+    this.subscription.unsubscribe();
   }
 }
